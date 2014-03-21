86fdd6ed75b483a24886ec6591cf3726ae4cb81a
zeXtreme <zerozwy@126.com>
2014-03-21 19:56:55 +0800

    20140321

--- Test/src/me/zwy/code/Test1.java
+++ Test/src/me/zwy/code/Test1.java
@@ -1,19 +1,30 @@
 package me.zwy.code;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Test1 {
 
 	public static void main(String[] args) {
-		List<String> list = new ArrayList<String>();
-		list.add("a");
-		list.add("b");
-		list.add("c");
-		list.add(0, list.remove(list.size()-1));
-		for(String s : list){
-			System.out.println(s);
-		}
+		System.out.println(print("1111"));
+	}
+	
+	public static int gcd(int x,int y)
+	{
+	 if(x<y)
+	 {
+	  int tmp = x;
+	  x = y;
+	  y = tmp;
+	 }
+	 if(x%y==0)
+	 {
+	  return y;
+	 }else
+	 {
+	  return gcd(y,x%y);
+	 }
+	}
+	
+	public static String print(String str){
+		return str;
 	}
 
 }
